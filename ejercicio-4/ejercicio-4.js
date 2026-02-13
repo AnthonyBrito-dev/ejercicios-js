03e438ee9fdf789c47e85c446c677aa444d05e5a
AnthonyBrito-dev <anthonyrafaelbritobravo10@gmail.com>
2026-02-13 14:56:34 -0500

    Se añade los comentarios a las partes importantes del codigo

--- ejercicio-4/ejercicio-4.js
+++ ejercicio-4/ejercicio-4.js
@@ -1,8 +1,9 @@
 //Seleccionamos el input con la busqueda y los li los metemos en una lista
 let busqueda = document.getElementById("buscar")
 let ulLis = document.querySelectorAll("li")
-let lista1 = []
+let lista1 = [] // se le añadiran los textos de cada <li>
 
+//Se agrega el evento al input de busqueda 
 busqueda.addEventListener("keyup", ()=> {
     let texto2 = busqueda.value.toUpperCase()
     
@@ -12,7 +13,9 @@ busqueda.addEventListener("keyup", ()=> {
         }
     }
 
+    //Se filtra primero el array que contiene c/u de los textos
     lista1.filter(subArray => {
+        //Se filtra cada elemento para buscar si contiene la palabra
         if (subArray.includes(texto2)){
             for (let newItem of ulLis){
                 if (newItem.innerHTML.includes(texto2)){
